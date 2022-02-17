f6a1f257a7969d2cb225df0bbe35f45ad33b32ba
notapie <notapie@yandex.ru>
2022-02-17 19:01:50 +0300

    first solution

--- optional.h
+++ optional.h
@@ -15,35 +15,155 @@ template <typename T>
 class Optional {
 public:
     Optional() = default;
-    Optional(const T& value);
-    Optional(T&& value);
-    Optional(const Optional& other);
-    Optional(Optional&& other);
 
-    Optional& operator=(const T& value);
-    Optional& operator=(T&& rhs);
-    Optional& operator=(const Optional& rhs);
-    Optional& operator=(Optional&& rhs);
+    Optional(const T& value) {
+        // Сначала конструируем объект в выделенной под него памяти
+        value_ptr_ = new (&data_[0]) T(value);
+        // Затем ставим метку о том, что optional не пуст
+        is_initialized_ = true;
+    }
 
-    ~Optional();
+    Optional(T&& value) {
+        // Конструируем пустой объект нужного типа в выделенной памяти
+        value_ptr_ = new (&data_[0]) T(std::move(value));
+        // Затем ставим метку о том, что optional не пуст
+        is_initialized_ = true;
+    }
 
-    bool HasValue() const;
+    Optional(const Optional& other) {
+        // Если копируемый объект не инициализирован, то нет смысла дальше что-то делать
+        if (!other.is_initialized_) {
+            return;
+        }
+        CopyFrom(other);
+    }
+
+    Optional(Optional&& other) {
+        // Если копируемый объект не инициализирован, то нет смысла дальше что-то делать
+        if (!other.is_initialized_) {
+            return;
+        }
+        MoveFrom(other);
+    }
+
+    Optional& operator=(const T& value) {
+        if (!is_initialized_) {
+            // Сначала конструируем объект в выделенной под него памяти
+            value_ptr_ = new (&data_[0]) T(value);
+            // Затем ставим метку о том, что optional не пуст
+            is_initialized_ = true;
+        } else {
+            *value_ptr_ = value;
+        }
+        return *this;
+    }
+    Optional& operator=(T&& value) {
+        if (!is_initialized_) {
+            // Сначала конструируем объект в выделенной под него памяти
+            value_ptr_ = new (&data_[0]) T(std::move(value));
+            // Затем ставим метку о том, что optional не пуст
+            is_initialized_ = true;
+        } else {
+            *value_ptr_ = std::move(value);
+        }
+        return *this;
+    }
+    Optional& operator=(const Optional& rhs) {
+        if (!rhs.is_initialized_) {
+            if (is_initialized_) {
+                Reset();
+            }
+        } else {
+            CopyFrom(rhs);
+        }
+        return *this;
+    }
+    Optional& operator=(Optional&& rhs) {
+        if (!rhs.is_initialized_) {
+            if (is_initialized_) {
+                Reset();
+            }
+        } else {
+            MoveFrom(rhs);
+        }
+        return *this;
+    }
+
+    ~Optional() {
+        Reset();
+    }
+
+    bool HasValue() const {
+        return is_initialized_;
+    }
 
     // Операторы * и -> не должны делать никаких проверок на пустоту Optional.
     // Эти проверки остаются на совести программиста
-    T& operator*();
-    const T& operator*() const;
-    T* operator->();
-    const T* operator->() const;
+    T& operator*() {
+        return *value_ptr_;
+    }
+    const T& operator*() const {
+        return *value_ptr_;
+    }
+    T* operator->() {
+        return value_ptr_;
+    }
+    const T* operator->() const {
+        return value_ptr_;
+    }
 
     // Метод Value() генерирует исключение BadOptionalAccess, если Optional пуст
-    T& Value();
-    const T& Value() const;
+    T& Value() {
+        return const_cast<T&>(
+            const_cast<const Optional<T>*>(this)->Value()
+        );
+    }
 
-    void Reset();
+    const T& Value() const {
+        if (!is_initialized_) {
+            throw BadOptionalAccess();
+        }
+        return *value_ptr_;
+    }
+
+    void Reset() {
+        if (!is_initialized_) {
+            return;
+        }
+        value_ptr_->~T();
+        value_ptr_ = nullptr;
+        is_initialized_ = false;
+    }
 
 private:
     // alignas нужен для правильного выравнивания блока памяти
     alignas(T) char data_[sizeof(T)];
+    T* value_ptr_ = nullptr;
     bool is_initialized_ = false;
+
+    void CopyFrom(const Optional& other) {
+        // Если этот объект не инициализирован, создаём и копируем
+        // Иначе просто копируем
+        // Вариант, что копируемый объект не инициализирован отсекается до входа в этот метод
+        if (!is_initialized_) {
+            // Конструируем объект, перемещая только что скопированный
+            value_ptr_ = new (&data_[0]) T(*other.value_ptr_);
+            // И теперь ставим метку
+            is_initialized_ = true;
+        } else {
+            *value_ptr_ = *other.value_ptr_;
+        }
+    }
+
+    void MoveFrom(Optional& other) {
+        if (!is_initialized_) {
+            // Конструируем объект, перемещая только что скопированный
+            value_ptr_ = new (&data_[0]) T(std::move(*other.value_ptr_));
+            // И теперь ставим метку
+            is_initialized_ = true;
+        } else {
+            *value_ptr_ = std::move(*other.value_ptr_);
+        }
+    }
+
 };
